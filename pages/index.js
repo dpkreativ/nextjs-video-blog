9c7089d76fe42692b9da1eeffb94cb0cad74be6e
Divine Orji <dpkreativ@gmail.com>
2021-06-21 00:16:28 +0100

    display blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,45 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 import { promises as fs } from "fs";
 import path from "path";
 import grayMatter from "gray-matter";
+
+export default function Home({ posts }) {
+  return (
+    <div>
+      <Head>
+        <title>Video Blog</title>
+        <meta name="description" content="A Simple video blog" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>A Video Blog</h1>
+        <div className={styles.grid}>
+          {posts.map((post) => {
+            const { title, path, author, date, description } = post;
+            return (
+              <Link key={path} href={path}>
+                <a>
+                  <div className={styles.card}>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    <p>
+                      {author} | <span>{date}</span>
+                    </p>
+                    <Link href={path}>
+                      <a>Read more &rarr;</a>
+                    </Link>
+                  </div>
+                </a>
+              </Link>
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
+}
 
 export async function getStaticProps() {
   const postDirectory = path.join(process.cwd(), "pages/blogposts");

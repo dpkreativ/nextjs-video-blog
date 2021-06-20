fbb027cb28c0d431aacaead4a1a84048cf46abc9
Divine Orji <dpkreativ@gmail.com>
2021-06-20 16:40:15 +0100

    custom config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withMdxEnhanced = require("next-mdx-enhanced");
+
+module.exports = withMdxEnhanced({
+  layoutPath: "./layouts",
+  defaultLayout: true,
+  fileExtensions: ["mdx"],
+  usesSrc: false,
+})();

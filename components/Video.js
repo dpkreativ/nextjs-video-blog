c48e74f5c30ebb3a9a6ae2c24f6ef03d65527ccd
Divine Orji <dpkreativ@gmail.com>
2021-06-20 21:43:26 +0100

    style for Video component

--- components/Video.js
+++ components/Video.js
@@ -7,6 +7,23 @@ const Video = ({ src }) => {
         allowFullScreen
         frameBorder="0"
       ></iframe>
+
+      <style jsx>
+        {`
+          .iframe-container {
+            overflow: hidden;
+            padding-top: 56.25%;
+            position: relative;
+          }
+          .iframe-container iframe {
+            height: 100%;
+            width: 100%;
+            left: 0;
+            position: absolute;
+            top: 0;
+          }
+        `}
+      </style>
     </div>
   );
 };
